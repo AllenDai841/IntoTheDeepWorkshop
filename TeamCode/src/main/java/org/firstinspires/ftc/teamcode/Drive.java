c9c69a4e516310092c84ae8c22eeb4cb5c938396
Allen Dai <allendai000@gmail.com>
2026-08-24 16:53:12 -0700

    reggin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp
+public class Drive extends OpMode{
+
+    @Override
+    public void init() {
+        //stuff goes here
+    }
+
+    @Override
+    public void loop() {
+        //more stuff goes here
+    }
+}
