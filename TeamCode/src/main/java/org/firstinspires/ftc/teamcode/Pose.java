d01bdb88870db651fc1d1ae3105ea146edfbde65
Allen Dai <allendai000@gmail.com>
2026-08-24 17:09:41 -0700

    reggin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pose.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Pose {
+
+    double x;
+    double y;
+    double heading;
+    public Pose(double x, double y, double heading){
+        this.x = x;
+        this.y = y;
+        this.heading = heading;
+    }
+}
