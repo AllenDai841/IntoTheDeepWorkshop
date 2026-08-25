8549a5adaeeb2ba8b3866a69eb8b759e15bab7f5
Allen Dai <allendai000@gmail.com>
2026-08-24 22:06:09 -0700

    regginator

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Limelight.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Limelight {//theres not enough time to make this one so its gonna be placeholder
+    public Limelight(){
+
+    }
+    public Pose getSample(){
+        return new Pose(0, 0, 0);
+    }
+}
