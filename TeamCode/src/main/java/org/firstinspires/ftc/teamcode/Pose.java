ff2d9ca7a4d206d1e40d540d8772cbfd38171ec2
merr <henry.bridgen@students.tka.org>
2026-08-24 17:29:30 -0700

    changed pose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pose.java
@@ -2,12 +2,27 @@ package org.firstinspires.ftc.teamcode;
 
 public class Pose {
 
-    double x;
-    double y;
-    double heading;
-    public Pose(double x, double y, double heading){
+    public double x;
+    public double y;
+    public double heading;
+
+    public Pose(double x, double y, double heading) {
         this.x = x;
         this.y = y;
         this.heading = heading;
+    }
+
+    public Pose copy() {
+        return new Pose(x, y, heading);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "X: %.2f | Y: %.2f | Heading: %.2f°",
+                x,
+                y,
+                Math.toDegrees(heading)
+        );
     }
 }
