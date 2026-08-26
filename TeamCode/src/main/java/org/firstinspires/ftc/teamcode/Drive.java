af76bf26d4fdef274f002fb3d8f3b071755445a7
merr <henry.bridgen@students.tka.org>
2026-08-25 17:25:16 -0700

    drive fix push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -1,17 +1,56 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-@TeleOp
-public class Drive extends OpMode{
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-    @Override
-    public void init() {
-        //stuff goes here
+public class Drive{
+    private DcMotorEx fl;
+    private DcMotorEx fr;
+    private DcMotorEx bl;
+    private DcMotorEx br;
+    public Drive(HardwareMap hardwareMap){
+        fl = hardwareMap.get(DcMotorEx.class, "fl");
+        fr = hardwareMap.get(DcMotorEx.class, "fr");
+        bl = hardwareMap.get(DcMotorEx.class, "bl");
+        br = hardwareMap.get(DcMotorEx.class, "br");
     }
 
-    @Override
-    public void loop() {
-        //more stuff goes here
+    public void drive(double forward, double strafe, double turn) {
+
+        double flPower = forward + strafe + turn;
+        double frPower = forward - strafe - turn;
+        double blPower = forward - strafe + turn;
+        double brPower = forward + strafe - turn;
+
+        double maxValue = Math.max(Math.abs(flPower), Math.max(Math.abs(frPower), Math.max(Math.abs(blPower), Math.abs(brPower))));
+        if(maxValue > 1){
+            flPower = flPower/maxValue;
+            frPower = frPower/maxValue;
+            blPower = blPower/maxValue;
+            brPower = brPower/maxValue;
+        }
+        fl.setPower(flPower);
+        fr.setPower(frPower);
+        bl.setPower(blPower);
+        br.setPower(brPower);
+    }
+    public void goToPose(Pose targetPose, Pose currentPose) {
+
+        double changeX = targetPose.x - currentPose.x;
+        double changeY = targetPose.y - currentPose.y;
+        double changeH = targetPose.heading - currentPose.heading;
+
+        double heading = currentPose.heading;
+
+        double forward = changeX * Math.sin(heading)
+                + changeY * Math.cos(heading);
+
+        double strafe = changeX * Math.cos(heading)
+                - changeY * Math.sin(heading);
+
+        double constant = 0.05;
+
+        drive(forward * constant, strafe * constant, changeH * constant);
     }
 }
